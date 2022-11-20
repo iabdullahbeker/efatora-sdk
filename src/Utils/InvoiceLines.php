35983d1c4b0d6733678f05f38e3d5b43d325b149
Abdullah Mohamed <abdullah.mohamed@tecbadia.com>
2022-11-20 16:36:10 +0200

    update

--- src/Utils/InvoiceLines.php
+++ src/Utils/InvoiceLines.php
@@ -25,7 +25,7 @@ class InvoiceLines
         $this->total += ($item['quantity'] * $item['unitValue']['amountEGP']);
         // $this->total_discount = 0;
         // $tax = ($item->item_quantity * $item->item_price_each * 0.14);
-        $this->total_tax += $item['taxableItems']['amount'];
+        $this->total_tax += $item['taxableItems'][0]['amount'];
 
         array_push($this->items,$item);
     }
